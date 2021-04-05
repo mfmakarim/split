40dff822328868cacb59a47504ffa731befebaa4
fuadmakarim <fuadmakarim@gmail.com>
2021-04-05 09:11:44 +0700

    init general page

---
+++ android/lib/ui/pages/pages.dart
@@ -0,0 +1,6 @@
+import 'package:android/shared/shared.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:supercharged/supercharged.dart';
+
+part 'general_page.dart';
